eed40a83fb05b694bdaa0f9a61e647aa03c5317a
Yianni (John) Mercer <YJMercer@gmail.com>
2022-12-05 18:09:09 -0600

    Bug fixes -- corrected email function

--- secret_santa_generator.py
+++ secret_santa_generator.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os, sys
+import numpy as np
 import random
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -39,16 +40,33 @@ def secret_santa(participants_dict):
     """
     # GET NAMES OF EVERYONE INVOLVED
     names = list(participants_dict.keys())
+    result = []
+    restart = True
 
-    # RANDOMLY SELECT TWO PEOPLE FROM THE LIST, SAVE THE PARTNERS, AND KEEP GOING UNTIL THERE IS NO ONE LEFT
-    partners_list = []
-    for i in range(int(len(names)/2)):
-        partners = random.sample(names, 2)
-        names = list(set(names) - set(partners))
-        partners_list.append(tuple(partners))
-    
-    # RETURN PARTNERS LIST
-    return partners_list
+    while restart:
+        restart = False
+        receivers = names[:]
+
+        for i in range(len(names)):
+            giver = names[i]
+            # Pick a random reciever
+            receiver = random.choice(receivers)
+
+            # If we've got to the last giver and its the same as the reciever, restart the generation
+            if (giver == receiver and i == (len(names) - 1)):
+                restart = True
+                break
+            else:
+                # Ensure the giver and reciever are not the same, and they are not in the excludes list
+                while (receiver == giver):
+                    receiver = random.choice(receivers)
+                # Add result to array
+                result.append(tuple([giver, receiver]))
+                # Remove the reciever from the list
+                receivers.remove(receiver)
+    return result     
+
+
 
 def email_participants(people_dictionary, partners_list, path_to_template):
     """Email each participant who they have for Secret Santa
@@ -63,38 +81,9 @@ def email_participants(people_dictionary, partners_list, path_to_template):
     
     # ITERATE THROUGH PARTNER LIST AND SEND EMAILS
     for person1, person2 in partners_list:
-        # SENDING PERSON 1 EMAILS
         
         # FORMAT THE TEMPLATE MESSAGE 
         ss_filled = ss_template.format(person1, person2)
-        
-        # GET EMAIL AND PASSWORD TO SEND OUT THE MESSAGES
-        load_dotenv() # YOU'LL NEED TO CONFIGURE A .env FILE THAT CONTAINS THE BELOW VARIABLES WITH RESPECTIVE VALUES
-        sender_email = os.environ.get('sender_address')
-        sender_pass = os.environ.get('sender_pass')
-        
-        # SET UP THE MIME sets up the MIME
-        message = MIMEMultipart()
-        message['From'] = sender_email # MY EMAIL ADDRESS | FROM ME
-        message['To'] =  people_dictionary[person2] # RECEIVER OF EMAIL | TO YOU
-        message['Subject'] = 'Secret Santa 2022' # SUBJECT OF THE EMAIL
-        
-        # SET THE BODY OF THE EMAIL
-        message.attach(MIMEText(ss_filled, 'plain'))
-
-        # CREATE THE SMTP SESSION FOR SENDING THE EMAILScreates the SMTP session for sending the mail
-        session = smtplib.SMTP('smtp.gmail.com', 587)
-        session.connect("smtp.gmail.com", 587)
-        session.ehlo()
-        session.starttls()
-        session.login(sender_email, sender_pass)
-        text = message.as_string()
-        session.sendmail(sender_email, people_dictionary[person2], text)
-        
-        # SENDING PERSON 2 EMAILS
-        
-        # FORMAT THE TEMPLATE MESSAGE 
-        ss_filled = ss_template.format(person2, person1)
         
         # GET EMAIL AND PASSWORD TO SEND OUT THE MESSAGES
         load_dotenv() # YOU'LL NEED TO CONFIGURE A .env FILE THAT CONTAINS THE BELOW VARIABLES WITH RESPECTIVE VALUES
@@ -118,14 +107,17 @@ def email_participants(people_dictionary, partners_list, path_to_template):
         session.login(sender_email, sender_pass)
         text = message.as_string()
         session.sendmail(sender_email, people_dictionary[person1], text)
-        session.quit()
-            
+        session.quit() 
+           
 if __name__ == "__main__":
     approval_status = "NOT APPROVED"
+    people_email_dict = None
+    partners = None
     while approval_status == "NOT APPROVED":
         people_email_dict = read_ss_list(str(sys.argv[1]))
         partners = secret_santa(people_email_dict)
-        print(partners)
+        for name in partners:
+            print(name)
         approval = input("PLEASE APPROVE THE ABOVE SECRET SANTA PAIRINGS: ")
         if approval == 'APPROVED':
             email_participants(people_email_dict, partners, "secret_santa_template.txt")
